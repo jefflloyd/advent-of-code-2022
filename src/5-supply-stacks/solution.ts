1bc862582be822ac74fdecb1a2f20df2a6939ab3
Jeff Lloyd <jlloyd.su@gmail.com>
2022-12-12 14:13:55 -0800

    Added day 5 solution

---
+++ src/5-supply-stacks/solution.ts
@@ -0,0 +1,88 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+type Crates = Array<string>;
+type Stacks = Array<Crates>;
+
+const filename = path.join(__dirname, '../../src/5-supply-stacks/input.txt');
+const data = fs.readFileSync(filename, 'utf-8').split('\n');
+
+const stacksForCrateMover9000: Stacks = [];
+const stacksForCrateMover9001: Stacks = [];
+let initializedStacks = false;
+
+const parseStackLine = (line: string) => {
+  const characters = line.split('');
+
+  characters.forEach((char: string, index: number) => {
+    const charCode = char.charCodeAt(0);
+
+    if (charCode > 64 && charCode < 91) {
+      const stackToAddTo = (index - 1) / 4;
+
+      while (stackToAddTo >= stacksForCrateMover9000.length) {
+        stacksForCrateMover9000.push([]);
+        stacksForCrateMover9001.push([]);
+      }
+
+      stacksForCrateMover9000[stackToAddTo].push(char);
+      stacksForCrateMover9001[stackToAddTo].push(char);
+    }
+  });
+}
+
+const updateStacksForCrateMover9000 = (stacks: Stacks, numCratesToMove: number, from: number, to: number): void => {
+  for (let i = 0; i < numCratesToMove; i++) {
+    const removedCrate = stacks[from].shift();
+    
+    stacksForCrateMover9000[to].unshift(removedCrate);
+  }
+};
+
+const updateStacksForCrateMover9001 = (stacks: Stacks, numCratesToMove: number, from: number, to: number): void => {
+  const removedCrates: Crates = [];
+
+  for (let i = 0; i < numCratesToMove; i++) {
+    const removedCrate = stacksForCrateMover9001[from].shift();
+
+    removedCrates.push(removedCrate);
+  }
+
+  stacksForCrateMover9001[to].unshift(...removedCrates);
+};
+
+const parseMoveCrateInstruction = (line: string) => {
+  const splitLine = line.split(' ');
+  const numCratesToMove = parseInt(splitLine[1], 10);
+  const fromStackIndex = parseInt(splitLine[3], 10) - 1;
+  const toStackIndex = parseInt(splitLine[5], 10) - 1;
+
+  updateStacksForCrateMover9000(stacksForCrateMover9000, numCratesToMove, fromStackIndex, toStackIndex);
+  updateStacksForCrateMover9001(stacksForCrateMover9001, numCratesToMove, fromStackIndex, toStackIndex);  
+};
+
+data.forEach(line => {
+  if (!line.length) {
+    initializedStacks = true;
+  } else if (!initializedStacks) {
+    parseStackLine(line);
+  } else {
+    parseMoveCrateInstruction(line);
+  }
+});
+
+const getTopCratesForStacks = (stacks: Stacks): string => {
+  let stringifiedTopCrates = '';
+
+  stacks.forEach(stack => {
+    stringifiedTopCrates += stack[0];
+  });
+
+  return stringifiedTopCrates;
+};
+
+// Part 1:
+console.log(getTopCratesForStacks(stacksForCrateMover9000));
+
+// Part 2:
+console.log(getTopCratesForStacks(stacksForCrateMover9001));

516ef5786ce1418bc3a469198d93c78f747c4e41
Jeff Lloyd <jlloyd.su@gmail.com>
2022-12-13 17:20:04 -0800

    Added solution for part 2 of day 12, which included cleaning up part 1's code a bit

--- src/12-hill-climbing-algorithm/solution.ts
+++ src/12-hill-climbing-algorithm/solution.ts
@@ -6,19 +6,27 @@ type Coordinate = {
   y: number,
 };
 
-const STARTING_CELL = "S";
-const ENDING_CELL = "E";
+type ClimbingMode = {
+  ascending: boolean,
+  startingCell: 'S' | 'E',
+  endingCell: 'E' | 'a',
+};
+
+type PathToExplore = {
+  coordinate: Coordinate,
+  pathLength: number,
+};
 
 const filename = path.join(__dirname, '../../src/12-hill-climbing-algorithm/input.txt');
 const data = fs.readFileSync(filename, 'utf-8').split('\n');
 const grid = data.map(line => line.split(''));
 
-const findStartingCoordinate = (): Coordinate => {
+const findStartingCoordinate = (mode: ClimbingMode): Coordinate => {
   let coordinate: Coordinate;
 
   grid.forEach((row, rowIndex) => {
     row.forEach((cell, columnIndex) => {
-      if (cell === STARTING_CELL) {
+      if (cell === mode.startingCell) {
         coordinate = {
           x: columnIndex,
           y: rowIndex,
@@ -30,7 +38,7 @@ const findStartingCoordinate = (): Coordinate => {
   return coordinate;
 };
 
-const canTraverseToCell = (from: Coordinate, to: Coordinate): boolean => {
+const canTraverseToCell = (from: Coordinate, to: Coordinate, mode: ClimbingMode): boolean => {
   if (
     from.y < 0 || from.y >= grid.length || to.y < 0 || to.y >= grid.length ||
     from.x < 0 || from.x >= grid[0].length || to.x < 0 || to.x >= grid[0].length
@@ -40,60 +48,64 @@ const canTraverseToCell = (from: Coordinate, to: Coordinate): boolean => {
 
   const fromCellValue = grid[from.y][from.x];
   const toCellValue = grid[to.y][to.x];
+  const fromCharCode = fromCellValue.charCodeAt(0);
+  const toCharCode = toCellValue.charCodeAt(0);
 
-  if (fromCellValue === STARTING_CELL) {
-    return toCellValue === 'a' || toCellValue === 'b';
-  } else if (toCellValue === ENDING_CELL) {
-    return fromCellValue === 'y' || fromCellValue === 'z';
-  } else if (toCellValue === STARTING_CELL) {
-    return false;
+  if (mode.ascending) {
+    if (fromCellValue === mode.startingCell) {
+      return ['a', 'b'].includes(toCellValue);
+    } else if (fromCellValue === mode.endingCell) {
+      return ['y', 'z'].includes(toCellValue);
+    } else {
+      return toCharCode - fromCharCode < 2;
+    }
   } else {
-    const fromCharCode = fromCellValue.charCodeAt(0);
-    const toCharCode = toCellValue.charCodeAt(0);
-
-    return toCharCode - fromCharCode < 2;
+    if (fromCellValue === mode.startingCell) {
+      return ['y', 'z'].includes(toCellValue);
+    } else if (fromCellValue === mode.endingCell) {
+      return toCellValue === 'b';
+    } else {
+      return fromCharCode - toCharCode < 2;
+    }
   }
 };
 
-const startingCoordinate = findStartingCoordinate();
-const visitedCells = [];
-const priorityQueue = [];
-
-priorityQueue.push({
-  coordinate: startingCoordinate,
-  pathLength: 0,
-});
-
-const recurse = (coordinate: Coordinate, pathLength: number) => {
+const exploreCoordinate = (
+  coordinate: Coordinate,
+  pathLength: number,
+  mode: ClimbingMode,
+  visitedCells: Array<string>,
+  priorityQueue: Array<PathToExplore>,
+) => {
   const x = coordinate.x;
   const y = coordinate.y;
   const cell = grid[y][x];
 
-  if (cell === ENDING_CELL) {
+  if (cell === mode.endingCell) {
     return pathLength;
   } else {
-    if (canTraverseToCell(coordinate, { x: x - 1, y }) && !visitedCells.includes(`(${x - 1},${y})`)) {
+    if (canTraverseToCell(coordinate, { x: x - 1, y }, mode) && !visitedCells.includes(`(${x - 1},${y})`)) {
       visitedCells.push(`(${x - 1},${y})`);
       priorityQueue.push({
         coordinate: { x: x - 1, y },
         pathLength: pathLength + 1,
       });
     }
-    if (canTraverseToCell(coordinate, { x: x + 1, y }) && !visitedCells.includes(`(${x + 1},${y})`)) {
+    if (canTraverseToCell(coordinate, { x: x + 1, y }, mode) && !visitedCells.includes(`(${x + 1},${y})`)) {
       visitedCells.push(`(${x + 1},${y})`);
       priorityQueue.push({
         coordinate: { x: x + 1, y },
         pathLength: pathLength + 1,
       });
     }
-    if (canTraverseToCell(coordinate, { x, y: y - 1 }) && !visitedCells.includes(`(${x},${y - 1})`)) {
+    if (canTraverseToCell(coordinate, { x, y: y - 1 }, mode) && !visitedCells.includes(`(${x},${y - 1})`)) {
       visitedCells.push(`(${x},${y - 1})`);
       priorityQueue.push({
         coordinate: { x, y: y - 1 },
         pathLength: pathLength + 1,
       });
     }
-    if (canTraverseToCell(coordinate, { x, y: y + 1 }) && !visitedCells.includes(`(${x},${y + 1})`)) {
+    if (canTraverseToCell(coordinate, { x, y: y + 1 }, mode) && !visitedCells.includes(`(${x},${y + 1})`)) {
       visitedCells.push(`(${x},${y + 1})`);
       priorityQueue.push({
         coordinate: { x, y: y + 1 },
@@ -105,16 +117,54 @@ const recurse = (coordinate: Coordinate, pathLength: number) => {
   return -1;
 };
 
-let result: number;
+const findShortestPathLength = (mode: ClimbingMode): number => {
+  const startingCoordinate = findStartingCoordinate(mode);
+  const priorityQueue = [];
+  const visitedCells = [];
+  let finishedPathLength: number;
 
-while (priorityQueue.length) {
-  const newAttempt = priorityQueue.shift();
-  result = recurse(newAttempt.coordinate, newAttempt.pathLength);
+  priorityQueue.push({
+    coordinate: startingCoordinate,
+    pathLength: 0,
+  });
+  visitedCells.push(`(${startingCoordinate.x},${startingCoordinate.y})`);
 
-  if (result > -1) {
-    break;
+  while (priorityQueue.length) {
+    const newAttempt = priorityQueue.shift();
+    finishedPathLength = exploreCoordinate(
+      newAttempt.coordinate,
+      newAttempt.pathLength,
+      mode,
+      visitedCells,
+      priorityQueue,
+    );
+  
+    if (finishedPathLength > -1) {
+      break;
+    }
   }
-}
+
+  return finishedPathLength;
+};
+
+const ascendingMode: ClimbingMode = {
+  ascending: true,
+  startingCell: 'S',
+  endingCell: 'E',
+};
+
+// the problem seems to imply that the way to finish this problem is to explore all start points,
+// that is, all cells with 'a', and see which is the shortest to the top. but this way is much easier:
+// instead, do the exact same "climb", except descending from the finish point to the first 'a' you see.
+// this is a BFS just like the ascending mode, meaning the first path we find is guaranteed shortest.
+const descendingMode: ClimbingMode = {
+  ascending: false,
+  startingCell: 'E',
+  endingCell: 'a',
+};
 
 // Part 1:
-console.log(result);
+console.log(findShortestPathLength(ascendingMode));
+
+// Part 2:
+console.log(findShortestPathLength(descendingMode));

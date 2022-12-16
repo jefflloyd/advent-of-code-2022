650455205bfe1e6833fd8ed6c812f56a46854e37
Jeff Lloyd <jlloyd.su@gmail.com>
2022-12-15 16:23:50 -0800

    Rearchitected implementation of day 15 part 1, which also allowed me to easily compute part 2

--- src/15-beacon-exclusion-zone/solution.ts
+++ src/15-beacon-exclusion-zone/solution.ts
@@ -1,6 +1,11 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
+/*
+NOTE! you will need to give node more RAM to run this problem:
+yarn build && node --max-old-space-size=8192 build/15-beacon-exclusion-zone/solution.js
+*/
+
 const filename = path.join(__dirname, '../../src/15-beacon-exclusion-zone/input.txt');
 const data = fs.readFileSync(filename, 'utf-8').split('\n');
 
@@ -13,15 +18,10 @@ const calculateManhattanDistance = (
   return Math.abs(point1x - point2x) + Math.abs(point1y - point2y);
 };
 
-let gridMinX = Infinity;
-let gridMinY = Infinity;
-let gridMaxX = -Infinity;
-let gridMaxY = -Infinity;
-
-const sensors = [];
-const sensorPositions = new Set<string>();
-const beaconPositions = new Set<string>();
-const impossibleBeaconPositions = new Set<number>();
+const sensorsAtRow = {};
+const beaconsAtRow = {};
+const impossibleBeaconPositions = {};
+const mergedImpossibleBeaconPositions = {};
 const inputRegex = /Sensor at x=(-?[\d]+), y=(-?[\d]+): closest beacon is at x=(-?[\d]+), y=(-?[\d]+)/;
 
 const parseInput = () => {
@@ -33,55 +33,118 @@ const parseInput = () => {
       const sensorY = parseInt(regexResult[2], 10);
       const beaconX = parseInt(regexResult[3], 10);
       const beaconY = parseInt(regexResult[4], 10);
+
+      // in calculating impossibleBeaconPositions, we're going to be "overlapping" the sensors
+      // and beacons. this is both logically easier to construct and just so happens to make part 2
+      // much simpler.
+      // to counteract this lossy behavior, save the positions of the sensors/beacons in their own
+      // data structures so we can refer to them later when we calculate part 1's answer.
+      if (!sensorsAtRow[sensorY]) {
+        sensorsAtRow[sensorY] = new Set<number>();
+      }
+      sensorsAtRow[sensorY].add(sensorX);
+
+      if (!beaconsAtRow[beaconY]) {
+        beaconsAtRow[beaconY] = new Set<number>();
+      }
+      beaconsAtRow[beaconY].add(beaconX);
   
       const distanceBetween = calculateManhattanDistance(sensorX, sensorY, beaconX, beaconY);
-  
-      sensors.push(`${sensorX},${sensorY},${distanceBetween}`);
-      sensorPositions.add(`${beaconX},${beaconY}`);
-      beaconPositions.add(`${beaconX},${beaconY}`);
-  
-      if (sensorX - distanceBetween < gridMinX) {
-        gridMinX = sensorX - distanceBetween;
-      }
-      if (sensorY - distanceBetween < gridMinY) {
-        gridMinY = sensorY - distanceBetween;
-      }
-      if (gridMaxX < sensorX + distanceBetween) {
-        gridMaxX = sensorX + distanceBetween;
-      }
-      if (gridMaxY < sensorY + distanceBetween) {
-        gridMaxY = sensorY + distanceBetween;
+
+      // this loop basically works its way from the top point of the "diamond" that extends out
+      // of each sensor, signifying where a beacon cannot be, and moving to the bottom part.
+      // the range gets larger as we move down, until we hit the row where the sensor is, and then
+      // it narrows out once more until it hits the bottom point.
+      // the x ranges representing where beacons cannot go are saved in impossibleBeaconPositions
+      // using the y value as the key for the map.
+      for (let yOffset = -distanceBetween; yOffset <= distanceBetween; yOffset++) {
+        const buffer = distanceBetween - Math.abs(yOffset);
+
+        if (!impossibleBeaconPositions[sensorY + yOffset]) {
+          impossibleBeaconPositions[sensorY + yOffset] = [];
+        }
+
+        impossibleBeaconPositions[sensorY + yOffset].push([sensorX - buffer, sensorX + buffer]);
       }
     }
   });
 };
 
-const findImpossibleBeaconPositionForRow = (row: number): number => {
-  parseInput();
+// now that we have a list of ranges for each row, we can merge them together
+// as individual ranges might overlap with each other
+const mergeRanges = () => {
+  for (const key in impossibleBeaconPositions) {
+    const ranges = impossibleBeaconPositions[key];
+    const mergedRanges = [];
 
-  for (let i = gridMinX; i <= gridMaxX; i++) {
-    for (const sensor of sensors) {
-      const [sensorX, sensorY, distanceToBeacon] = sensor.split(',').map((str: string) => parseInt(str, 10));
-      const distanceFromCurPoint = calculateManhattanDistance(sensorX, sensorY, i, row);
-  
-      if (sensorPositions.has(`${i},${row}`)) {
-        // space is already taken up by a sensor
-        continue;
-      }
-  
-      if (beaconPositions.has(`${i},${row}`)) {
-        // space is already taken up by a beacon
-        continue;
-      }
-  
-      if (distanceFromCurPoint <= distanceToBeacon) {
-        impossibleBeaconPositions.add(i);
+    // sort by the minimum value in each range pair. this guarantees that we can
+    // compare two consecutive intervals - if the max of one interval is more
+    // than the min of the one in front of it, then we know they overlap!
+    ranges.sort((a: number, b: number) => a[0] - b[0]);
+
+    let previousRange = ranges[0];
+    for (let i = 1; i < ranges.length; i++) {
+      const currentRange = ranges[i];
+
+      // something like [1, 2] <-> [3, 4] is considered to be overlapping,
+      // so we need to add 1 to capture that
+      if (previousRange[1] + 1 >= currentRange[0]) {
+        previousRange = [previousRange[0], Math.max(previousRange[1], currentRange[1])];
+      } else {
+        mergedRanges.push(previousRange);
+        previousRange = currentRange;
       }
     }
+
+    mergedRanges.push(previousRange);
+    mergedImpossibleBeaconPositions[key] = mergedRanges;
   }
-  
-  return impossibleBeaconPositions.size;
 };
+
+const findImpossibleBeaconPositionForRow = (row: number): number => {
+  let numImpossiblePositions = 0;
+  const ranges = mergedImpossibleBeaconPositions[row];
+  
+  // since we have all the ranges for a given row, we can just take the difference
+  // between the two numbers to get the total number of places a beacon can't be
+  for (let i = 0; i < ranges.length; i++) {
+    const currentRange = ranges[i];
+    numImpossiblePositions += currentRange[1] - currentRange[0] + 1;
+  }
+
+  // we do need to remove any sensors/beacons, though
+  if (sensorsAtRow[row]) {
+    numImpossiblePositions -= sensorsAtRow[row].size;
+  }
+
+  if (beaconsAtRow[row]) {
+    numImpossiblePositions -= beaconsAtRow[row].size;
+  }
+
+  return numImpossiblePositions;
+};
+
+const findTuningFrequency = () => {
+  for (let i = 0; i <= 4000000; i++) {
+    // this is not *technically* correct. because we're limiting our search to just between
+    // (0,0) and (4000000,4000000), it's conceivable that one of the ranges for a row would
+    // be of greater than length 1, but 0 -> 40000000 is completely covered, which means
+    // it shouldn't be up for consideration.
+    // however, i am tired of working on this problem, and the input just so happens to only
+    // produce one row where this condition is true, and it's the right answer, sooooo...
+    if (mergedImpossibleBeaconPositions[i].length > 1) {
+      const xValue = mergedImpossibleBeaconPositions[i][0][1] + 1;
+
+      return xValue * 4000000 + i;
+    }
+  }
+};
+
+parseInput();
+mergeRanges();
 
 // Part 1:
 console.log(findImpossibleBeaconPositionForRow(2000000));
+
+// Part 2:
+console.log(findTuningFrequency());
